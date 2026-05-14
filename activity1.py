119d65c49305fa2e48ad93830d2b3f525e7edd6f
madhuksharas35176-blip <madhuksharas_35176@chettinadvidyashram.org>
2026-05-14 16:58:16 +0530

    Add files via upload

--- activity1.py
+++ activity1.py
@@ -1,7 +1,4 @@
-char = input("Enter a character: ")
-
-# Checking if the character is an alphabet
-if char.isalpha():
-    print(f"'{char}' is an alphabet.")
-else:
-    print(f"'{char}' is not an alphabet.")
+n = 5 
+for i in range(1, n + 1):
+    
+    print(" " * (n - i) + "*" * i)
